71e402a51613eccd883ac580cc7165c305e2ca11
JhonHCanoB <jhon.canob@campusucc.edu.co>
2025-08-06 01:12:04 +0000

    interface

--- Builder/Converter.java
+++ Builder/Converter.java
@@ -1,20 +1,7 @@
-public class Converter {
+interface Converter {
 
-    private String document;
+    public void makeLine();
+    public void makeParagraph();
+    public void makeTable();
 
-    public void makeLine(){
-
-    }
-
-    public void makeParagraph(){
-
-    }
-
-    public void makeTable(){
-
-    }
-
-    public String getDocument(){
-        return "";
-    }
 }
